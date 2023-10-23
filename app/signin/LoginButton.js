d81de9f96ca3bc0b18c9db66bd522b936029af9a
faisalmit@2502 <faisalirfan2502@gmail.com>
2023-10-23 19:50:03 +0530

    h

--- app/signin/LoginButton.js
+++ app/signin/LoginButton.js
@@ -1,51 +1,53 @@
-"use client"
-
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation'; 
 
 const LoginButton = () => {
   const router = useRouter();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const token = localStorage.getItem('token')
 
-    fetch('http://localhost:8080/graphql', {
-      method: 'POST',
-      headers: {
-        Authorization: 'bearer ' + token,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        query: `
-          query {
-            authentication
-          }
-        `,
-      }),
-    })
-    .then(res=>{
-      return res.json();
-    }).then(res => {
-      console.log(res);
-      if(res.erros && res.errors[0].status===420){
+  useEffect(() => {
+    // Ensure this code only runs on the client side
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      fetch('http://localhost:8080/graphql', {
+        method: 'POST',
+        headers: {
+          Authorization: 'bearer ' + token,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          query: `
+            query {
+              authentication
+            }
+          `,
+        }),
+      })
+      .then(res => res.json())
+      .then(res => {
+        if (res.errors && res.errors[0].status === 420) {
+          setIsAuthenticated(false);
+        } else if (res.errors) {
+          console.log(res.errors);
+          throw new Error('User login failed');
+        } else {
+          setIsAuthenticated(true);
+        }
+      })
+      .catch(error => {
+        console.error(error);
         setIsAuthenticated(false);
-      }
-      if (res.errors) {
-        console.log(res.errors);
-        throw new Error('User login failed');
-      }
-      setIsAuthenticated(true);
-    })
-    .catch(error => {
-      console.error(error);
-      setIsAuthenticated(false);
-    });// Make sure to include an empty dependency array here
+      });
+    }
+  }, []); // Empty dependency array ensures this effect runs only once
 
   const handleSignOut = (event) => {
     event.preventDefault();
     localStorage.removeItem('token');
     setIsAuthenticated(false);
     router.push('/signout');
-    localStorage.removeItem('email')
+    localStorage.removeItem('email');
   };
 
   const handleClick = (event) => {
@@ -58,7 +60,6 @@ const LoginButton = () => {
       onClick={isAuthenticated ? handleSignOut : handleClick}
       className="btn btn-outline-primary mr-2"
     >
-    {/* {console.log(isAuthenticated + "isAuthenticated value")} */}
       {isAuthenticated ? 'Sign Out' : 'Sign In'}
     </button>
   );
